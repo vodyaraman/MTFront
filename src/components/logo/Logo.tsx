c408fa5cb8a694ad2b4b82aadb710c91259d812e
vodyaraman <a.vodyaraman@gmail.com>
2025-04-06 20:39:39 +0300

    About/Pages

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,8 +3,8 @@ import "./Logo.scss";
 const Logo = () => {
   return (
     <div className="logo" draggable='false'>
-      <img src="/icons/main-icon.svg" alt="?" />
-      <h2 className="logo__text">METAL TRADING</h2>
+      <img src="/icons/main-icon.svg" alt="?" className="logo__icon"/>
+      <h2 className="logo__text">металл трейдинг</h2>
     </div>
   );
 };
