6b282e6d83190fc3b92674002f25d71417766bbd
vodyaraman <a.vodyaraman@gmail.com>
2025-04-09 22:09:40 +0300

    About refactoring

---
+++ src/components/about-components/AnimateClients.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import fts from "@/assets/icons/about/clients/fts.svg";
+import mmtp from "@/assets/icons/about/clients/mmtp.svg";
+import pind from "@/assets/icons/about/clients/p-ind.svg";
+import tec from "@/assets/icons/about/clients/tec.svg";
+import GrassTexture from "./Block";
+import "./AboutComponents.scss";
+
+const logos = [
+  { src: fts.src, alt: "Логотип ФТС" },
+  { src: mmtp.src, alt: "Логотип ММТП" },
+  { src: pind.src, alt: "Логотип Р-Индустрия" },
+  { src: tec.src, alt: "Логотип ТЭЦ" },
+];
+
+export default function ClientsSlider() {
+  const trackRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(max-width: 768px)").matches) return;
+
+    const track = trackRef.current;
+    if (!track) return;
+
+    const height = track.offsetHeight / 2;
+
+    const startAnimation = () => {
+        const totalHeight = track.scrollHeight / 2;
+    
+        gsap.to(track, {
+          y: `-=${totalHeight}`,
+          duration: 30,
+          ease: "none",
+          repeat: -1,
+          modifiers: {
+            y: gsap.utils.unitize((y) => parseFloat(y) % totalHeight),
+          },
+        });
+      };
+    
+      // Ожидаем отрисовку всех изображений + DOM
+      requestAnimationFrame(() => {
+        setTimeout(startAnimation, 0); // или 50–100ms для надёжности
+      });
+
+    console.log("Slider started")
+  }, []);
+
+  return (
+      <div className="slider-track" ref={trackRef}>
+        {[...logos, ...logos].map((logo, index) => (
+          <GrassTexture key={index}>
+            <img src={logo.src} alt={logo.alt} className="inside inside--clients" />
+          </GrassTexture>
+        ))}
+      </div>
+  );
+}
